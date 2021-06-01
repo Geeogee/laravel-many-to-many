aba7c9bdc6127eb79d75f2b2e8c7d760810db829
Gioele <gioele.armiento@gmail.com>
2021-06-01 17:51:19 +0200

    Exercise done

--- laravel/resources/views/layouts/main-layout.blade.php
+++ laravel/resources/views/layouts/main-layout.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('title')
+    {{-- Google Font --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"> 
+    {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -9,11 +9,38 @@
 @section('main')
     <main>
         <div class="container">
-            <ul>
+            <ul class="employee-list">
                 @foreach ($employees as $employee)
-                    <li>
-                        {{ $employee -> firstname }}
-                        {{ $employee -> lastname }}
+                    <li class="employee">
+                        <div class="employee-infos">
+                            {{ $employee -> firstname }}
+                            {{ $employee -> lastname }}
+                        </div>
+                        <ul class="employee-location">
+                            <li>
+                                <h4 class="employee-title">Location:</h4>
+                            </li>
+                            <li>
+                                {{-- There's no need to use foreach, because an employee 
+                                can only have one location --}}
+                                {{ $employee -> location -> address }}
+                                {{ $employee -> location -> state }}
+                            </li>
+                        </ul>
+
+                        <ul class="employee-tasks">
+                            <li class="tasks-count">
+                                <h4 class="employee-title">Tasks ({{ $employee -> tasks -> count() }}):</h4>
+                                <ol>
+                                    @foreach ($employee -> tasks as $task)
+                                        <li>
+                                            {{ $task -> name }}
+                                        </li>
+                                    @endforeach
+                                </ol>
+                            </li>
+                            
+                        </ul>
                     </li>
                 @endforeach
             </ul>
